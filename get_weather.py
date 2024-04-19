53e6604f5e7cc96f0271430e16554e155e0d1bc2
Nikita Smirnov <job.nvsmirnov@gmail.com>
2024-04-19 16:11:19 +0300

    'get_weather' - updated: 8 methods are declared + 1 is updated
    Объявил следующие методы:
    	get_beautiful_cityname_str() - возвращает строку в указанном формате с названием города;
    	get_beautiful_weather_description_str() - возвращает строку в указанном формате с описанием погоды;
    	get_beautiful_temperature_str() - возвращает строку в указанном формате со значением температуры;
    	get_beautiful_humidity_str() - возвращает строку в указанном формате со значением влажности;
    	get_beautiful_pressure_str() - возвращает строку в указанном формате со значением давления;
    	get_beautiful_wind_description_str() - возвращает строку в указанном формате с описанием ветра;
    	get_beautiful_goodbye_message_str() - возвращает строку в указанном формате с сообщением о прощании;
    	get_beautiful_datetime_str() - возвращает строку в указанном формате с датой и временем.
    
    В методе get_weather() теперь извлекается больше данных.
    Переведено с помощью DeepL.com (бесплатная версия)

--- get_weather.py
+++ get_weather.py
@@ -1,13 +1,65 @@
 import requests
-
+import datetime
 from cfg import TGBOT_TOKEN
 
+
+def get_beautiful_cityname_str(city: str) -> str:
+    ''' Функция формирует строку с названием города'''
+    pass
+
+
+def get_beautiful_weather_description_str(weather_description: str, sunrise: datetime.time, sunset: datetime.time) -> str:
+    ''' Функция формирует строку с описанием погоды '''
+    pass
+
+
+def get_beautiful_temperature_str(temperature: float) -> str:
+    ''' Функция формирует строку со значением температуры'''
+    pass
+
+
+def get_beautiful_humidity_str(humidity: float) -> str:
+    ''' Функция формирует строку со значением влажности'''
+    pass
+
+
+def get_beautiful_pressure_str(pressure: float) -> str:
+    ''' Функция формирует строку со значением давления'''
+    pass
+
+
+def get_beautiful_wind_description_str(wind_speed: float) -> str:
+    ''' Функция формирует строку с данными о ветре'''
+    pass
+
+
+def get_beautiful_goodbye_message_str() -> str:
+    ''' Функция формирует строку с обращением к пользователю'''
+    pass
+
+
+def get_beautiful_datetime_str() -> str:
+    ''' Функция формирует строку со значениями даты и времени'''
+    pass
+
+
 def get_weather(city: str):
+    # запрос
     request = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={TGBOT_TOKEN}&units=metric')
     response = request.json()
 
+    # для проверки времени суток (get_beautiful_weather_description(arg, sunrise, sunset))
+    sunrise = datetime.datetime.fromtimestamp(response['sys']['sunrise']).time()
+    sunset = datetime.datetime.fromtimestamp(response['sys']['sunset']).time()
+
+    # извлечение нужных данных из ответа на запрос
     city = response['name']
-    print(city)
+    weather_description = response['weather'][0]['main']
+    humidity = response['main']['humidity']
+    pressure = response['main']['pressure']
+    wind_description = response['wind']['speed']
+    goodbye_message = get_beautiful_goodbye_message_str()
+    today = get_beautiful_datetime_str()
 
-
-get_weather('Москва')
+    # печать ответа пользователю
+    print(city, weather_description, humidity)
